2a36403187b06c8f0c435a2190ff0a373d016944
Rui Monteiro <ruiseixas@yahoo.com>
2023-08-23 12:23:45 +0100

    Variance

---
+++ CSharp/CovarianceContravariance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Variance; // Covariance and Contravariance are two forms of Variance
+
+public class Animal { }
+
+public class Cat : Animal { } // Cat is a subtype of Animal
+
+public class Program
+{
+    public static Animal CovariantMethod ()
+    {
+        Console.WriteLine("Covariance!");
+        return new Cat ();
+    }
+
+    public static void ContravariantMethod(Animal animal)
+    {
+        Console.WriteLine("Contravariance!");
+    }
+
+    static void Main(string[] args)
+    {
+
+        CovariantMethod(); // Covariance
+        ContravariantMethod(new Cat()); // Contravariance
+
+        Console.ReadKey();
+    }
+}
