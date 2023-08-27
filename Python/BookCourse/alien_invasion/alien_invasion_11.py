31a2a232459fadb3bf5ad9acd17691ebdc5dd6d6
Rui Monteiro <ruiseixas@yahoo.com>
2023-08-28 00:35:02 +0100

    Bullets

--- Python/BookCourse/alien_invasion/alien_invasion_11.py
+++ Python/BookCourse/alien_invasion/alien_invasion_11.py
@@ -1,7 +1,8 @@
 import sys
 import pygame
-from settings_2 import Settings
+from settings_3 import Settings #v.11
 from ship_3 import Ship # v.7
+from bullet import Bullet # v.11
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -18,6 +19,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group() # v.11
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -25,6 +27,7 @@ class AlienInvasion:
             # Refactoring into a method
             self._check_events() # v.4
             self.ship.update() # v.6
+            self.bullets.update() # v.11
             self._update_screen() # v.4
 
     def _check_events(self): # v.4
@@ -48,6 +51,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE: # v.10
             sys.exit()
+        elif event.key == pygame.K_SPACE: # v.11
+            self._fire_bullet()
         
     def _check_keyup_events(self, event): # v.8
         """Respond to keypresses."""
@@ -56,10 +61,17 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT: # v.7
             self.ship.moving_left = False
 
+    def _fire_bullet(self): # v.11
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self): # v.4
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites(): # v.11
+            bullet.draw_bullet()
 
         # Make the most recently draw screen visible.
         pygame.display.flip()
