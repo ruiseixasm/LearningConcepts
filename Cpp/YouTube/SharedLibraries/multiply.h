25676a3c9effa1169b24d676aa07205c855ef5b8
ruiseixasm <ruiseixas@yahoo.com>
2024-03-27 21:54:32 +0000

    Shared Libraries

---
+++ Cpp/YouTube/SharedLibraries/multiply.h
@@ -0,0 +1,11 @@
+// Interface for our multiply function
+// By: Nick from CoffeeBeforeArch
+
+#pragma once
+
+/* Multiplies two numbers
+ * @param a The first number to multiply
+ * @param b The second number to multiply
+ * @return The multiplication of a and b
+ */
+int multiply(int a, int b);
