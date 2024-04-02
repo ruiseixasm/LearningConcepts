6fb111453845960c0626436325bc6ec514c9dc71
ruiseixasm <ruiseixas@yahoo.com>
2024-04-02 08:47:57 +0100

    Condition Variable

--- Cpp/YouTube/CppNuts/Multithreading/11-condition-variable/condition_variable.cpp
+++ Cpp/YouTube/CppNuts/Multithreading/11-condition-variable/condition_variable.cpp
@@ -31,3 +31,32 @@ std::condition_variable cv;
 std::mutex m;
 long balance = 0;
 
+void addMoney(int money) {
+    std::lock_guard<mutex> lg(m);   // scoped kind of lock
+    balance += money;
+    cout << "Amount Added to Current Balance: " << balance << endl;
+    cv.notify_one();    // awakes the cv.wait bellow!
+}
+
+void withdrowMoney(int money) {
+    std::unique_lock<mutex> ul(m);  // Locking the common mutex m
+    // Unlocks m while waiting!
+    cv.wait(ul, [] { return (balance != 0); });  // false - waits, true - continues
+    if (balance >= money) {
+        balance -= money;
+        cout << "Amount Deducted: " << money << endl;
+    } else {
+        cout << "Amount Can't Be Deducted, Current Balance Is Less Than " << money << endl;
+    }
+    cout << "Current Balance Is: " << balance << endl;
+}
+
+int main() {
+    std::thread t1(withdrowMoney, 500);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::thread t2(addMoney, 500);
+    t1.join();
+    t2.join();
+
+    return 0;
+}
