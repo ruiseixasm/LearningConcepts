c020e5af47d7551b0ed1ebbaf142e679740af007
ruiseixasm <ruiseixas@yahoo.com>
2024-04-03 00:32:12 +0100

    unique lock

--- Cpp/YouTube/CppNuts/Multithreading/10-unique-lock/unique_lock.cpp
+++ Cpp/YouTube/CppNuts/Multithreading/10-unique-lock/unique_lock.cpp
@@ -24,11 +24,30 @@ std::mutex m1;
 int buffer = 0;
 
 // EXAMPLE 1
-
-void task(const char* threadName, int loopFor) {
+void task_1(const char* threadName, int loopFor) {
     std::unique_lock<mutex> lock(m1);   // Automatically calls lock on mutex m1
-    for (int i = 0; i < loopFor; ++i) {
-        buffer++;
-        cout << threadName << buffer << endl;
-    }
+    for (int i = 0; i < loopFor; ++i)
+        cout << threadName << ++buffer << endl;
+}
+
+// EXAMPLE 2
+void task_2(const char* threadName, int loopFor) {
+    std::unique_lock<mutex> lock(m1, std::defer_lock);   // Doesn't call lock on mutex m1
+    lock.lock();    // defer_lock requires the explicit lock afterwards
+    for (int i = 0; i < loopFor; ++i)
+        cout << threadName << ++buffer << endl;
+    // lock.unlock(); is not necessary as it will be called by the unique_lock destructor
+}
+
+int main() {
+    thread t1a(task_1, "T1A ", 10);
+    thread t1b(task_1, "T1B ", 10);
+    thread t2a(task_2, "T2A ", 10);
+    thread t2b(task_2, "T2B ", 10);
+    t1a.join();
+    t1b.join();
+    t2a.join();
+    t2b.join();
+
+    return 0;
 }
