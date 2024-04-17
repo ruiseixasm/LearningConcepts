a9a98f16c946ca6bbe61041599d4412ad5ae8c34
Rui Monteiro <ruiseixas@yahoo.com>
2024-04-17 02:16:27 +0100

    bags

---
+++ C/Books/ElementosProgramacaoC/08-Conjuntos/05-multi-sets-bags.c
@@ -0,0 +1,12 @@
+// Compile command: gcc 05-multi-sets-bags.c bags.c -DSET_MAX=255 -o 05-multi-sets-bags.out
+
+#include <stdio.h>
+#include "sets.h"
+
+
+int main()
+{
+    
+    
+    return 0;
+}
