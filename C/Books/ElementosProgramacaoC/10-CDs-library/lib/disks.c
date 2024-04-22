cc49b9e615222d795f76af7f50f4a7e5b73f5631
Rui Monteiro <ruiseixas@yahoo.com>
2024-04-22 06:12:54 +0100

    lib

--- C/Books/ElementosProgramacaoC/10-CDs-library/lib/disks.c
+++ C/Books/ElementosProgramacaoC/10-CDs-library/lib/disks.c
@@ -1,4 +1,63 @@
 #include "disks.h"
 
 
+Disk MakeDisk(Artist a, Title t, Year y, Style s)
+{
+    Disk d;
+    d.artist    = a;
+    d.title     = t;
+    d.year      = y;
+    d.style     = s;
+    d.n_songs   = 0;
+    return d;
+}
 
+void BuildDisk(Disk *d, Artist a, Title t, Year y, Style s)
+{
+    d->artist   = a;
+    d->title    = t;
+    d->year     = y;
+    d->style    = s;
+    d->n_songs  = 0;
+}
+
+void AddSong(Disk *d, Song s)
+{
+    d->songs[(d->n_songs)++] = s;
+}
+
+void SetArtist(Disk *d, Artist a)
+{
+    d->artist   = a;
+}
+
+void SetTitle(Disk *d, Title t)
+{
+    d->title    = t;
+}
+
+void SetYear(Disk *d, Year y)
+{
+    d->year     = y;
+}
+
+void SetStyle(Disk *d, Style s)
+{
+    d->style    = s;
+}
+
+void SetDisk(Disk *d, Artist a, Title t, Year y, Style s)
+{
+    d->artist   = a;
+    d->title    = t;
+    d->year     = y;
+    d->style    = s;
+}
+
+void RemoveSong(Disk *d, short n)
+{
+    // Songs are enumerated from 1 and not from 0 (n > 0)!
+    for (size_t i = n; i < d->n_songs; i++)
+        d->songs[i - 1] = d->songs[i];
+    d->n_songs--;
+}
--- C/Books/ElementosProgramacaoC/10-CDs-library/lib/disks.h
+++ C/Books/ElementosProgramacaoC/10-CDs-library/lib/disks.h
@@ -3,5 +3,33 @@
 
 #include "songs.h"
 
+typedef char *Artist;
+typedef char *Title;
+typedef short Year;
+typedef short Style;
+
+#define NUMBER_OF_STYLES    6
+#define MAX_SONGS           24
+
+typedef struct {
+    Artist  artist;
+    Title   title;
+    Year    year;
+    Style   style;
+    short   n_songs;
+    Song    songs[MAX_SONGS];
+} Disk;
+
+Disk MakeDisk(Artist a, Title t, Year y, Style s);
+void BuildDisk(Disk *d, Artist a, Title t, Year y, Style s);
+void AddSong(Disk *d, Song s);
+void SetArtist(Disk *d, Artist a);
+void SetTitle(Disk *d, Title t);
+void SetYear(Disk *d, Year y);
+void SetStyle(Disk *d, Style s);
+void SetDisk(Disk *d, Artist a, Title t, Year y, Style s);
+void RemoveSong(Disk *d, short n);
+
+
 
 #endif /* LIB_DISKS */
