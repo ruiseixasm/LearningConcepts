cc440a11f274d56124ee863306f1ae5e8565819e
Rui Monteiro <ruiseixas@yahoo.com>
2024-04-24 10:37:53 +0100

    long long int

--- C/ChatGPT/VoidPointers/02-array-pointers.c
+++ C/ChatGPT/VoidPointers/02-array-pointers.c
@@ -6,31 +6,34 @@
 
 void BuildPointers(void **v, const void *p, size_t n, size_t s)
 {
-    printf("n: %d, s: %d bytes, (int *)p: %p, (char *)p: %p, p: %p\n", n, s, (int *)p, (char *)p, p);
+    // *p   - Array of pointers to values, in this case, long long int values (8 bytes)
+    // **v  - Array of pointers to pointers, in this case, (void) addresses (always the same, 4 or 8 bytes)
+    printf("n: %d, s: %d bytes, (long long int *)p: %p, (char *)p: %p, p: %p\n", n, s, (long long int *)p, (char *)p, p);
     // Pointers are like a needle on a record groove, they need to be place on the right groove,
-    //     4 bytes means 4 grooves appart (i += s)
+    //     i += s   - 8 bytes offset for i means 8 grooves appart, because i is a pointer to a long long int value
+    //     *v++     - always 4 bytes offset for *v because it's a pointer to an address and not a value
     for (char *i = (char *)p; n; n--, i += s, *v++) // Size s, bytes, is in multiple of chars, that have 1 byte
     {
         *v = (void *)i;   // All pointers have integer size, Ex. 4 bytes, equally sized, even void addresses!
         printf("Address i: %p, Address *v: %p, Value i: %d, Value *v: %d\n",
-                        i, *v, *i, *((int *)*v));
+                        i, *v, *i, *((long long int *)*v));
     }
 }
 
 int main() {
     // Define an array of integers
-    int array[] = {10, 20, 30, 40, 50};
+    long long int array[] = {10, 20, 30, 40, 50};
 
     // Define an array of void pointers
     void *pointers[5];
 
     // Use the BuildPointers function to build pointers to the elements of the array
-    BuildPointers(pointers, array, 5, sizeof(int));
+    BuildPointers(pointers, array, 5, sizeof(long long int));
 
     // Print the addresses and values pointed to by the pointers
     printf("\nAddresses and Values:\n");
     for (int i = 0; i < 5; i++) {
-        printf("Pointer %d: Address=%p, Value=%d\n", i, pointers[i], *((int *)pointers[i]));
+        printf("Pointer %d: Address=%p, Value=%d\n", i, pointers[i], *((long long int *)pointers[i]));
     }
 
     return 0;
