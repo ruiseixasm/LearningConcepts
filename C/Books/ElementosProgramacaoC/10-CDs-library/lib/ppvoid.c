75cf2327c6dedc69490a580942efd90279e446d0
Rui Monteiro <ruiseixas@yahoo.com>
2024-04-24 02:32:04 +0100

    void pointers

--- C/Books/ElementosProgramacaoC/10-CDs-library/lib/ppvoid.c
+++ C/Books/ElementosProgramacaoC/10-CDs-library/lib/ppvoid.c
@@ -1,4 +1,76 @@
 #include "ppvoid.h"
 
+void SortPointers(void **v, size_t n, int(*f)(const void *, const void *))
+{
+    if (iRandom(2))
+        SortPointersBubble(v, n, f);
+    else
+        SortPointersQuick(v, n, f);    
+}
 
+void SortPointersBubble(void **v, size_t n, int(*f)(const void *, const void *))
+{
+    void *m;
+    for (size_t i = 1; i < n; i++)
+        for (size_t j = n - 1; j >= i; j--)
+            if (!f(v[j - 1], v[j]))
+            {
+                m = v[j - 1];
+                v[j - 1] = v[j];
+                v[j] = m;
+            }
+}
 
+void SortPointersQuick(void **v, size_t n, int(*f)(const void *, const void *))
+{
+    void **i = v;
+    void **j = v + (n - 1);
+    void *a = *(i + (j - i)/2); // middle position
+    void *m;
+    
+    do
+    {
+        while (!f(a, *i))
+            i++;
+        while (!f(*j, a))
+            j--;
+        if (i <= j)
+        {
+            m = *i;
+            *i++ = *j;
+            *j-- = m;
+        }
+    } while (i <= j);
+    
+    if (v < j)
+        SortPointersQuick(v, j - v + 1, f);
+    else
+        SortPointersQuick(i, v + n - i, f);
+}
+
+int RestrictPointers(void **v, size_t n, int(*f)(const void *))
+{
+    
+}
+
+void *FirstPointer(void **v, size_t n, int(*f)(const void *))     // returns void* pointer
+{
+    
+}
+
+// Enabling functions that convert multiple sized pointers to void pointers
+
+void BuildPointers(void **v, const void *p, size_t n, size_t s)
+{
+    
+}
+
+void ClearPointers(void **v, size_t n)
+{
+    
+}
+
+void IteratePointers(void **v, size_t n, void(*f)(void *))
+{
+    
+}
--- C/Books/ElementosProgramacaoC/10-CDs-library/lib/ppvoid.h
+++ C/Books/ElementosProgramacaoC/10-CDs-library/lib/ppvoid.h
@@ -1,6 +1,18 @@
 #ifndef LIB_PPVOID
 #define LIB_PPVOID
 
+#include <stddef.h>     // needed for size_t
+#include "utils.h"
 
+void SortPointers(void **v, size_t, int(*f)(const void *, const void *));
+void SortPointersBubble(void **v, size_t n, int(*f)(const void *, const void *));
+void SortPointersQuick(void **v, size_t n, int(*f)(const void *, const void *));
+int RestrictPointers(void **v, size_t n, int(*f)(const void *));
+void *FirstPointer(void **v, size_t n, int(*f)(const void *));    // returns void* pointer
+
+// Enabling functions that convert multiple sized pointers to void pointers
+void BuildPointers(void **v, const void *p, size_t n, size_t s);
+void ClearPointers(void **v, size_t n);
+void IteratePointers(void **v, size_t n, void(*f)(void *));
 
 #endif /* LIB_PPVOID */
