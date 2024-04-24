cd47d4aeb065e289ea98083a4be031200cdea991
Rui Monteiro <ruiseixas@yahoo.com>
2024-04-24 06:01:20 +0100

    Improved cmake script

--- C/Books/ElementosProgramacaoC/10-CDs-library/run_cmake.sh
+++ C/Books/ElementosProgramacaoC/10-CDs-library/run_cmake.sh
@@ -3,16 +3,19 @@
 # Check if the build folder exists
 if [ -d "build" ]; then
     echo "Build folder exists. Changing directory to it..."
-    cd build
-    # Run cmake commands
-    cmake ..
-    make
-    cd ..
-    echo "Files that will be considered in the next git commit..."
-    git ls-files --others --exclude-standard
 else
-    echo "cuild folder does not exist. Please create it first with 'mkdir build'..."
-    #echo "Build folder does not exist. Creating it..."
-    #mkdir build
-    #cd build
+    # echo "cuild folder does not exist. Please create it first with 'mkdir build'..."
+    echo "Build folder does not exist. Creating it..."
+    mkdir build
 fi
+
+# Run cmake commands
+cd build
+cmake ..
+make
+cd ..
+echo "Files that will be considered in the next git commit..."
+git ls-files --others --exclude-standard
+
+# Install linux cmake needed build-essential if cmake outputs errors:
+# sudo apt install build-essential
