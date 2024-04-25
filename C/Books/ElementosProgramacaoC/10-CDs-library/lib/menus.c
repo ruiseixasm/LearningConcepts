06fa61548ba669c00a8f3c1dd0f5cc8f388b0fcb
Rui Monteiro <ruiseixas@yahoo.com>
2024-04-25 11:14:14 +0100

    Menus lib

--- C/Books/ElementosProgramacaoC/10-CDs-library/lib/menus.c
+++ C/Books/ElementosProgramacaoC/10-CDs-library/lib/menus.c
@@ -1,4 +1,43 @@
 #include "menus.h"
 
+static void DisplayMenu(const Menu *m)
+{
+    for (int i = 0; i < m->n_Items; i++)
+        printf("(%2d) %s\n", i + 1, m->items[i]);
+    printf("--> ");
+}
+int MenuSelect(const Menu *m)
+{
+    char reply[80];
+    int n;
 
+    for (;;)
+    {
+        DisplayMenu(m);
+        read_input(reply, sizeof(reply));
 
+        if (!*reply)
+            return 0;
+        else if (isdigit(*reply) &&
+                (n = atoi(reply)) <= m->n_Items)
+            return n;
+        else
+            printf("\a");
+    }
+}
+void CreateMenu(Menu *m, const char *s)
+{
+    
+}
+void EnableItem(Menu *m, int x)
+{
+    
+}
+void DisableItem(Menu *m, int x)
+{
+    
+}
+void EnableItemCond(Menu *m, int x, int b)
+{
+
+}
--- C/Books/ElementosProgramacaoC/10-CDs-library/lib/menus.h
+++ C/Books/ElementosProgramacaoC/10-CDs-library/lib/menus.h
@@ -1,6 +1,24 @@
 #ifndef LIB_MENUS
 #define LIB_MENUS
 
+#include <stdio.h>
+#include <ctype.h>  // nedded for isdigit()
+#include <stdlib.h> // needed for atoi(), malloc(), calloc() and free()
+#include "utils.h"  // needed for read_input()
+
+#define MAX_MENU_ITEMS  16
+
+typedef struct {
+    char *items[MAX_MENU_ITEMS];
+    int n_Items;
+} Menu;
+
+static void DisplayMenu(const Menu *m);
+int MenuSelect(const Menu *m);
+void CreateMenu(Menu *m, const char *s);
+void EnableItem(Menu *m, int x);
+void DisableItem(Menu *m, int x);
+void EnableItemCond(Menu *m, int x, int b);
 
 
 #endif /* LIB_MENUS */
