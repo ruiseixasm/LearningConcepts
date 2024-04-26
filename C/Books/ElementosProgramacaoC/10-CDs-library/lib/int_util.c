84d530e011704c18c7df04ac770720d772b1c00f
Rui Monteiro <ruiseixas@yahoo.com>
2024-04-26 04:00:51 +0100

    int util

--- C/Books/ElementosProgramacaoC/10-CDs-library/lib/int_util.c
+++ C/Books/ElementosProgramacaoC/10-CDs-library/lib/int_util.c
@@ -1,4 +1,17 @@
 #include "int_util.h"
 
 
+int ispos(int a)
+{
+    return a > 0;
+}
 
+int min(int a, int b)
+{
+    return b < a ? b : a;
+}
+
+int max(int a, int b)
+{
+    return b > a ? b : a;
+}
--- C/Books/ElementosProgramacaoC/10-CDs-library/lib/int_util.h
+++ C/Books/ElementosProgramacaoC/10-CDs-library/lib/int_util.h
@@ -1,6 +1,8 @@
 #ifndef LIB_INT_UTIL
 #define LIB_INT_UTIL
 
-
+int ispos(int a);
+int min(int a, int b);
+int max(int a, int b);
 
 #endif /* LIB_INT_UTIL */
