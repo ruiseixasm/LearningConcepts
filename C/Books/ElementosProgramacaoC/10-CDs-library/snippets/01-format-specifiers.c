c7dc82baf804e98656dcfd4b2034573f6d48b218
Rui Monteiro <ruiseixas@yahoo.com>
2024-04-26 02:28:22 +0100

    buttons

--- C/Books/ElementosProgramacaoC/10-CDs-library/snippets/01-format-specifiers.c
+++ C/Books/ElementosProgramacaoC/10-CDs-library/snippets/01-format-specifiers.c
@@ -6,10 +6,13 @@ int main()
     char ones[] = "11111";
     char twos[] = "22222";
     
-    printf("%s%.*s", ones, 50, twos);
+    // 3 sets the maximum numbers of chars printed of twos (with '.')
+    printf("%s%.*s", ones, 3, twos);
+    printf("|\n");
+    
+    // 30 sets the minimum numbers of space occupied by twos (without '.')
+    printf("%s%*s", ones, 30, twos);
     printf("|\n");
     
     return 0;
 }
-
-
