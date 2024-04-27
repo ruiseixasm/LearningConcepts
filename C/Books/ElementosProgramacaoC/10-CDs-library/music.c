c93c546aa4b93ef3db7cffebc5d84d6ea68f17da
Rui Monteiro <ruiseixas@yahoo.com>
2024-04-27 03:32:18 +0100

    Added Menus to main function/file

--- C/Books/ElementosProgramacaoC/10-CDs-library/music.c
+++ C/Books/ElementosProgramacaoC/10-CDs-library/music.c
@@ -8,21 +8,52 @@
 #define UNABLE_TO_WRITE "Unable to write file. "
 #define UNABLE_TO_READ  "Unable to read file. "
 
-Menu mainMenu;
-Menu fileMenu;
-Menu diskMenu;
-Menu styleMenu;
-Menu searchMenu;
-Menu selectMenu;
+Menu mainMenu;      // A
+Menu fileMenu;      // B
+Menu diskMenu;      // C
+Menu styleMenu;     // D
+Menu searchMenu;    // E
+Menu sortMenu;      // F
+Menu selectMenu;    // G
 
-int dbOpen;
+int dbOpen;     // boolean
 char dbName[64];
 int searchApprox = 0;
+char style_name[64];    // needed for safe StyleNameOf(char *style_name, const Style s)
 
 typedef void (*fRestrict)(const char *);
 fRestrict RestrictByArtist[2]   = {RestrictByArtistFull, RestrictByArtistApprox};
 fRestrict RestrictByTitle[2]    = {RestrictByTitleFull, RestrictByTitleApprox};
 fRestrict RestrictBySong[2]     = {RestrictBySongFull, RestrictBySongApprox};
+
+// AUXILIAR
+int istrlen(const char *s);
+
+// MENUS
+int DoMainMenu(void);
+void DoFileMenu(void);
+void DoDisksMenu(void);
+void DoSearchMenu(void);
+void DoSortMenu(void);
+void DoSelectMenu(void);
+void DoDisplaySelection(void);
+
+// ITEMS
+void DoAddItems(void);
+void DoDisplayItems(void);
+void DoEditItems(void);
+void DoDeleteItems(void);
+
+// DISKS
+void DoEditDisk(Disk *d);
+int DoEditMainInfo(Disk *d);
+void DoEditSongs(Disk *d);
+
+// SONGS
+void DoEditOneSong(Disk *d);
+void DoDeleteOneSong(Disk *d);
+void DoInsertOneSong(Disk *d);
+void DoAddOneSong(Disk *d);
 
 int main(int argc, char *argv[])
 {
@@ -33,8 +64,135 @@ int main(int argc, char *argv[])
     printf("\n");
 
 
-    int i;
+    /* inicialização dos menus */
+    // Main Menu    (A)
+    CreateMenu(&mainMenu, "MAIN MENU", "file>>;disks>>;search>>;quit;");
+    // File Menu    (B)
+    CreateMenu(&fileMenu, "FILE", "new;open...;close;save;save as...;abandon;");
+    // Disk Menu    (C)
+    CreateMenu(&diskMenu, "DISKS", "new...;inspect...;edit...;delete...;");
+    // Styles Menu  (D)
     CreateMenu(&styleMenu, "Style: ", "");
+    for (int i = 1; i <= NUMBER_OF_STYLES; i++)
+        AppendItem(&styleMenu, StyleNameOf(style_name, i));
+    // Search Menu  (E)
+    CreateMenu(&searchMenu, "SEARCH", "restart;use aproximations;select>>;"
+                                      "sort>>;display;print...;;"); // Last element is the 7th
+    DisableItem(&searchMenu, 7);    // Disables last element (7th)
+    // Sort Menu    (F)
+    CreateMenu(&sortMenu, "sort by", "artist;title;year;");
+    // Select Menu  (G)
+    CreateMenu(&selectMenu, "select by", "artist...;title...;year...;style...;song...;");
+
+    ClearDataBase();
+    dbOpen = 0; // false
+    while (DoMainMenu()) {}
     
     return 0;
+}
+
+
+// AUXILIAR
+
+int istrlen(const char *s)
+{
+    return strlen(s);
+}
+
+// MENUS
+
+int DoMainMenu(void)
+{
+    return 0;
+}
+
+void DoFileMenu(void)
+{
+    
+}
+
+void DoDisksMenu(void)
+{
+    
+}
+
+void DoSearchMenu(void)
+{
+    
+}
+
+void DoSortMenu(void)
+{
+    
+}
+
+void DoSelectMenu(void)
+{
+    
+}
+
+void DoDisplaySelection(void)
+{
+    
+}
+
+// ITEMS
+
+void DoAddItems(void)
+{
+    
+}
+
+void DoDisplayItems(void)
+{
+    
+}
+
+void DoEditItems(void)
+{
+    
+}
+
+void DoDeleteItems(void)
+{
+    
+}
+
+// DISKS
+
+void DoEditDisk(Disk *d)
+{
+    
+}
+
+int DoEditMainInfo(Disk *d)
+{
+    return 0;
+}
+
+void DoEditSongs(Disk *d)
+{
+    
+}
+
+// SONGS
+
+void DoEditOneSong(Disk *d)
+{
+    
+}
+
+void DoDeleteOneSong(Disk *d)
+{
+    
+}
+
+void DoInsertOneSong(Disk *d)
+{
+    
+}
+
+void DoAddOneSong(Disk *d)
+{
+
 }
