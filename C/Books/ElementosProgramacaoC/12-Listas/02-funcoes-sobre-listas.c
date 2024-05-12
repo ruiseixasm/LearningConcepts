ec00b40db1d5b4a47d75d26d82d99de9153a31b7
Rui Monteiro <ruiseixas@yahoo.com>
2024-05-12 19:23:23 +0100

    Contar palavras

--- C/Books/ElementosProgramacaoC/12-Listas/02-funcoes-sobre-listas.c
+++ C/Books/ElementosProgramacaoC/12-Listas/02-funcoes-sobre-listas.c
@@ -1,3 +1,82 @@
+// Compile command: gcc 02-funcoes-sobre-listas.c -c
 #include "02-funcoes-sobre-listas.h"
 
 
+// 1. Funções básicas sobre listas ///////////////////////////////////////////
+
+List listcpy(List *s, constList t)                  /* duplicação           */
+{
+    List p;
+    if (t == NULL_LIST)
+        *s = NULL_LIST;
+    else
+    {
+        *s = listnew(t->value);
+        p = *s;
+        t = t->next;
+        while (t != NULL_LIST)
+        {
+            p->next = listnew(t->value);
+            p = p->next;
+            t = t->next;
+        }
+    }
+    return *s;
+}
+
+List listnew(Item x)                                /* nova linha unitária  */
+{
+    List p = (List)malloc(sizeof(Listnode));
+    p->value = x;
+    p->next = NULL_LIST;
+    return p;
+}
+
+List listclr(List *s)                               /* limpar a lista       */
+{
+    return *s = NULL_LIST;
+}
+
+List listcons(Item x, List *s)                      /* acrescentar à cabeça */
+{
+    List p = listnew(x);
+    p->next = *s;
+    return *s = p;
+}
+
+List liststhd(List s, Item x)                       /* modificar a cabeça   */
+{
+    s->value = x;
+}
+
+List listswtl(List s, List *t)                      /* troca a cauda        */
+{
+    List p;
+    p = s->next;
+    s->next = *t;
+    *t = p;
+    return s;
+}
+
+Item listhead(constList s)                          /* a cabeça             */
+{
+    return s->value;
+}
+
+List listtail(constList s)                          /* a cauda              */
+{
+    return s->next;
+}
+
+int  listlen(constList s)                           /* o comprimento        */
+{
+    int n;
+    for (n = 0; s != NULL_LIST; n++)
+        s = s->next;
+    return n;
+}
+
+int  listnull(constList s)                          /* é vazia?             */
+{
+    return s == NULL_LIST;
+}
--- C/Books/ElementosProgramacaoC/12-Listas/02-funcoes-sobre-listas.h
+++ C/Books/ElementosProgramacaoC/12-Listas/02-funcoes-sobre-listas.h
@@ -1,12 +1,16 @@
 #ifndef FUNC_LISTAS
 #define FUNC_LISTAS
 
-typedef int Item;
-//typedef char *Item;
+#include <stdlib.h>     // defines NULL
+
+//typedef int Item;
+typedef char *Item;
+typedef const char *constItem;
 
 // 1. Funções básicas sobre listas ///////////////////////////////////////////
 
 typedef struct lnode *List; // Apontador
+typedef const struct lnode *constList;
 typedef struct lnode {
     Item value;
     List next;
@@ -14,33 +18,34 @@ typedef struct lnode {
 
 #define NULL_LIST NULL      // NULL_LIST alias to NULL
 
-List listcpy(List *s, const Listnode *t);           /* duplicação           */
-List listnew(const Item x);                         /* nova linha unitária  */
+List listcpy(List *s, constList t);                 /* duplicação           */
+List listnew(Item x);                               /* nova linha unitária  */
 List listclr(List *s);                              /* limpar a lista       */
-List listcons(const Item x, List *s);               /* acrescentar à cabeça */
-List liststhd(List *s, const Item);                 /* modificar a cabeça   */
+List listcons(Item x, List *s);                     /* acrescentar à cabeça */
+List liststhd(List s, Item x);                      /* modificar a cabeça   */
+List listswtl(List s, List *t);                     /* troca a cauda        */
 
-Item listhead(const List *s);                       /* a cabeça             */
-List listtail(const List *s);                       /* a cauda              */
-int  listlen(const List *s);                        /* o comprimento        */
-int  listnull(const List *s);                       /* é vazia?             */
+Item listhead(constList s);                         /* a cabeça             */
+List listtail(constList s);                         /* a cauda              */
+int  listlen(constList s);                          /* o comprimento        */
+int  listnull(constList s);                         /* é vazia?             */
 
 // 2. Funções utilitárias sobre listas ///////////////////////////////////////
 
-List listsuff(List s, int n);                       /* sufixo               */
-List listinsx(const Item x, List s, int n);         /* Inserçaõ em posição  */
-List listlist(const Listnode *x, List *s, int n);   /* inserção de lista    */
-List listrm(List s, int n);                         /* remoção              */
-List listsplt(const Listnode *x, int n, List *s);   /* partição             */
-List listfoot(const Listnode *x);                   /* pé                   */
-List listcat(List *s, const Listnode *t);           /* concatenação         */
+//List listsuff(List s, int n);                       /* sufixo               */
+//List listinsx(const Item x, List s, int n);         /* Inserçaõ em posição  */
+//List listlist(const Listnode *x, List *s, int n);   /* inserção de lista    */
+//List listrm(List s, int n);                         /* remoção              */
+//List listsplt(const Listnode *x, int n, List *s);   /* partição             */
+//List listfoot(const Listnode *x);                   /* pé                   */
+//List listcat(List *s, const Listnode *t);           /* concatenação         */
 
 // 3. Funções de busca em listas /////////////////////////////////////////////
 
-List listmmbr(const Listnode *s, int n);    /* membro de uma lista          */
-List listsrch(const Listnode *s, int n);    /* busca em lista ordenada      */
-List listins(List *s, int n);               /* inserção em lista ordenada   */
-List listentr(List *s, int n);              /* idem, sem repetição          */
+//List listmmbr(const Listnode *s, int n);    /* membro de uma lista          */
+//List listsrch(const Listnode *s, int n);    /* busca em lista ordenada      */
+//List listins(List *s, int n);               /* inserção em lista ordenada   */
+//List listentr(List *s, int n);              /* idem, sem repetição          */
 
 
 #endif /* FUNC_LISTAS */
