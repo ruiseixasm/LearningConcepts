2dc5e78d605fc6c0b61ba43ac7a1800ace6f6271
Rui Monteiro <ruiseixas@yahoo.com>
2024-05-18 19:25:43 +0100

    Contar palavras

--- C/Books/ElementosProgramacaoC/13-Arvores/01-funcoes-arvores.c
+++ C/Books/ElementosProgramacaoC/13-Arvores/01-funcoes-arvores.c
@@ -1,3 +1,86 @@
 // Compile command: gcc 01-funcoes-arvores.c -c
 #include "01-funcoes-arvores.h"
 
+
+// 1. Funções básicas sobre ARVORES ///////////////////////////////////////////
+
+Tree treenew(Item x)
+{
+    Tree p = malloc(sizeof(Treenode));
+    p->value = x;
+    p->sub[left] = p->sub[right] = NULL_TREE;
+    return p;
+}
+
+Tree treeclr(Tree *s)
+{
+    return *s = NULL_TREE;
+}
+
+Tree treecpy(Tree *s, constTree t)
+{
+    if (t == NULL_TREE)
+        *s = NULL_TREE;
+    else
+    {
+        *s = treenew(t->value);
+        treecpy(&(*s)->sub[left], t->sub[left]);
+        treecpy(&(*s)->sub[right], t->sub[right]);
+    }
+    return *s;
+}
+
+Tree treecons(Item x, Tree *s, Child c)
+{
+    Tree p = malloc(sizeof(Treenode));
+    p->value = x;
+    p->sub[c] = *s;
+    p->sub[!c] = NULL_TREE; // complementar sub
+    return *s = p;
+}
+
+Tree treestrt(Tree s, Item x)
+{
+    s->value = x;
+    return s;
+}
+
+Tree treeswch(Tree s, Tree *t, Child c)
+{
+    Tree p;
+    p = s->sub[c];
+    s->sub[c] = *t;
+    *t = p;
+    return s;
+}
+
+// 2. Funções utilitárias sobre ARVORES ///////////////////////////////////////
+
+int  treeleaf(constTree s)
+{
+    return treenull(treechld(s, left)) && treenull(treechld(s, right));
+}
+
+int  treesize(constTree s)
+{
+    return treenull(s) ? 0 :
+            1 + treesize(treechld(s, left)) + treesize(treechld(s, right));
+}
+
+int  treehght(constTree s)
+{
+    return treenull(s) ? 0 :
+            1 + max(treehght(treechld(s, left)), treehght(treechld(s, right)));
+}
+
+int  max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+// 3. Percursos em ARVORES ////////////////////////////////////////////////////
+
+
+
+// 4. ARVORES de procura //////////////////////////////////////////////////////
+
--- C/Books/ElementosProgramacaoC/13-Arvores/01-funcoes-arvores.h
+++ C/Books/ElementosProgramacaoC/13-Arvores/01-funcoes-arvores.h
@@ -13,15 +13,44 @@ typedef const struct tnode *constTree;
 typedef struct tnode {
     Item value;
     Tree sub[2];
-} Treenode;                 // Tipo (Valor)
+} Treenode;
+typedef enum {left, right} Child;
 
 #define NULL_TREE NULL      // NULL_TREE alias to NULL
 
 
 // 1. Funções básicas sobre ARVORES ///////////////////////////////////////////
 
+Tree treenew(Item x);
+Tree treeclr(Tree *s);
+Tree treecpy(Tree *s, constTree t);
+Tree treecons(Item x, Tree *s, Child c);
+Tree treestrt(Tree s, Item x);
+//int  treenull(constTree s);
+//Tree treeroot(constTree s);
+//Tree treechld(constTree s, Child c);
+#define treenull(s)     (!(s))
+#define treerroot(s)    ((s)->value)
+#define treechld(s, c)  ((s)->sub[c])
+Tree treeswch(Tree s, Tree *t, Child c);
 
+// 2. Funções utilitárias sobre ARVORES ///////////////////////////////////////
 
+int  treeleaf(constTree s);
+int  treesize(constTree s);
+int  treehght(constTree s);
+int  max(int a, int b);
 
+// 3. Percursos em ARVORES ////////////////////////////////////////////////////
+
+void treepre(constTree s, void(*p)(Item));
+void treein(constTree s, void(*p)(Item));
+void treepost(constTree s, void(*p)(Item));
+
+// 4. ARVORES de procura //////////////////////////////////////////////////////
+
+Tree treemmbr(constTree s, constItem x, int(*f)(constItem, constItem));
+Tree treesrch(constTree s, constItem x, int(*f)(constItem, constItem));
+Tree treeins(Tree *s, Item x, int(*f)(constItem, constItem));
 
 #endif /* FUNC_ARVORES */
