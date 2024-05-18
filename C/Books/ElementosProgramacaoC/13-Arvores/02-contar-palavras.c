3cd8b97f610af4b38d0c67aada17949068f69860
Rui Monteiro <ruiseixas@yahoo.com>
2024-05-19 00:16:09 +0100

    contar palavras numa árvore

---
+++ C/Books/ElementosProgramacaoC/13-Arvores/02-contar-palavras.c
@@ -0,0 +1,17 @@
+// Compile command: gcc 02-contar-palavras.c 01-funcoes-arvores.c -g -o 02-contar-palavras.out
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "01-funcoes-arvores.h"
+
+
+
+
+int main()
+{
+
+
+    return 0;
+}
