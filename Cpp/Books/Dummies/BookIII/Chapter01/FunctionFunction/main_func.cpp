34a1d0061af2894923e72ea5698e2f6917957319
Rui Monteiro <ruiseixas@yahoo.com>
2024-06-07 14:25:03 +0100

    Vector of functions

---
+++ Cpp/Books/Dummies/BookIII/Chapter01/FunctionFunction/main_func.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int AddSome(int Value) {
+  return Value + 10;
+}
+
+int DelSome(int Value) {
+  return Value - 10;
+}
+
+int MulSome(int Value) {
+  return Value * 10;
+}
+
+int DivSome(int Value) {
+  return Value / 10;
+}
+
+typedef int(*FuncPtr)(int);
+
+void ModIt(int Value, vector<FuncPtr> FuncArray) {
+  int NumFunc = FuncArray.size();
+  cout << "Processing " << NumFunc << " functions." << endl;
+
+  for(int i = 0; i < NumFunc; i++) {
+    cout << FuncArray[i] << "\t";
+    cout << *FuncArray[i] << "\t";
+    cout << *(FuncArray[i]) << "\t";  // equivalent to the previous one!
+    cout << FuncArray[i](Value) << endl;
+  }
+}
+
+int main() {
+  vector<FuncPtr> FuncArray =       {AddSome, DelSome, MulSome, DivSome};
+  ModIt(10, FuncArray);
+  vector<FuncPtr> FuncArrayValue =  {*AddSome, *DelSome, *MulSome, *DivSome};
+  ModIt(10, FuncArrayValue);
+  return 0;
+}
