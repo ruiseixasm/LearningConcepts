d5a15812dc631e3d51eaffc60bcff1fbfe2d66b9
Rui Monteiro <ruiseixas@yahoo.com>
2024-07-03 05:50:56 +0100

    Without any template

---
+++ Cpp/Books/Dummies/BookVII/Chapter01/RandomAccess2/main_withouttemplate.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+float AddIt(vector<float>::iterator begin,
+            vector<float>::iterator end) {
+  float Sum = 0;
+  vector<float>::iterator Index;
+  // Make sure that the values are in the correct order.
+  if (begin > end)
+  {
+    vector<float>::iterator temp;
+    temp = begin;
+    begin = end;
+    end = temp;
+  }
+  for (Index = begin; Index != end; Index++)
+    Sum += *Index;
+  return Sum;
+}
+
+int main() {
+  vector<float> Numbers;
+  Numbers.push_back(1.0);
+  Numbers.push_back(2.5);
+  Numbers.push_back(3.75);
+  Numbers.push_back(1.26);
+  Numbers.push_back(9.101);
+  Numbers.push_back(11.3);
+  Numbers.push_back(1.52);
+
+  // Sum the individual members.
+  float Sum;
+  Sum = AddIt(Numbers.begin(), Numbers.end());
+  cout << Sum << endl;
+  Sum = AddIt(Numbers.end(), Numbers.begin());
+  cout << Sum << endl;
+
+  // Sum a range.
+  vector<float>::iterator Iter = Numbers.begin();
+  Iter += 5;
+  Sum = AddIt(Iter, Numbers.end());
+  cout << Sum << endl;
+  return 0;
+}
