99f25e3c29e35a5dc3394b8a6a138739cd4fe90b
Rui Monteiro <ruiseixas@yahoo.com>
2024-07-07 20:49:12 +0100

    Tokinizer

--- Cpp/Boost/Examples/tokenizer.cpp
+++ Cpp/Boost/Examples/tokenizer.cpp
@@ -8,6 +8,14 @@ int main() {
   string MyString = "This is a test string!";
   tokenizer<> Tokens(MyString);
 
+// tokenizer<> uses the default template parameter which is char_separator<char>,
+//     suitable for tokenizing strings by whitespace.
+// The default tokenizer template parameters are set up to use boost::char_separator<char>
+//     for whitespace separation.
+// This works because the tokenizer class template defaults to char_separator<char>
+//     if no template parameters are specified.
+
+
   // Display each token on screen.
   tokenizer<>::iterator Iterate;
   for (Iterate = Tokens.begin(); Iterate != Tokens.end();
