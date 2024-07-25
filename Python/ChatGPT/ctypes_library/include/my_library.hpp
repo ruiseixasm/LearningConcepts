dcbe2074759ecbd68165cbefe0051cdc02f3c3e8
Rui Monteiro <ruiseixas@yahoo.com>
2024-07-25 12:56:13 +0100

    Includes call convention __stdcall

--- Python/ChatGPT/ctypes_library/include/my_library.hpp
+++ Python/ChatGPT/ctypes_library/include/my_library.hpp
@@ -3,19 +3,21 @@
 
 #ifdef _WIN32
     #define DLL_EXPORT __declspec(dllexport)
+    #define CALL_CONV __stdcall
 #else
     #define DLL_EXPORT
+    #define CALL_CONV
 #endif
 
 /*
-    If <iostream> is included then ctypes thows an error,
+    If <iostream> is included then ctypes throws an error,
     if <iostream> isn't included then ctypes works just fine!
 */
 #include <iostream> // THIS SIMPLE INCLUSION MAKES CTYPES TO FAIL ON WINDOWS
 
 
 extern "C" {
-    DLL_EXPORT int add_ctypes(int a, int b);
+    DLL_EXPORT int CALL_CONV add_ctypes(int a, int b);
 }
 
 #endif // MY_LIBRARY_HPP
