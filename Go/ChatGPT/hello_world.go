9ddb05c3020c4e9c27518508767e3f9720f4e899
Rui Monteiro <ruiseixas@yahoo.com>
2024-12-11 14:15:07 +0000

    Added comments to the Hello World Go program

--- Go/ChatGPT/hello_world.go
+++ Go/ChatGPT/hello_world.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
 }
+
+// Run with the command "go run hello_world.go"
+// Create executable with the command "go build hello_world.go"
+// Run the executable file with "hello_world.go"
