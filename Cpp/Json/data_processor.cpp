ebf62214c241fee9ce103013c36a3d8f81adb12d
Rui Monteiro <ruiseixas@yahoo.com>
2025-04-01 00:29:35 +0100

    Working Cpp json extractor

--- Cpp/Json/data_processor.cpp
+++ Cpp/Json/data_processor.cpp
@@ -10,6 +10,9 @@
 // Include the JSON library
 #include "single_include/nlohmann/json.hpp"
 
+#include <sstream>
+#include <fstream>
+
 
 
 double get_float(int numerator_number, int denominator_number) {
@@ -25,13 +28,25 @@ int main() {
     int numerator_number = 10;
     int denominator_number = 123456;
 
-    
-    nlohmann::json json_files_data = nlohmann::json::parse("list_of_floats_3.json");
+
+    std::stringstream json_files_buffer;
+
+    const char* filename = "list_of_floats_3.json";
+    std::ifstream json_file(filename);
+    if (!json_file.is_open()) {
+        std::cerr << "Could not open the file: " << filename << std::endl;
+        return 1;
+    }
+    json_files_buffer << json_file.rdbuf();
+    json_file.close();
+        
+    std::string json_files_list = json_files_buffer.str();
+    nlohmann::json json_files_data = nlohmann::json::parse(json_files_list.c_str());
 
     for (nlohmann::json jsonFloat : json_files_data) {
 
         double json_float = jsonFloat;
-        std::cout << "Extracted float :" << json_float << std::endl;
+        std::cout << "Extracted float: " << json_float << std::endl;
 
     }
 
