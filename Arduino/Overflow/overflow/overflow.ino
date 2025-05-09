1885b202d6ab169d20f6d80db8af67316890e185
Rui Monteiro <ruiseixas@yahoo.com>
2025-05-09 02:31:01 +0100

    Done overflow test with commented results

--- Arduino/Overflow/overflow/overflow.ino
+++ Arduino/Overflow/overflow/overflow.ino
@@ -1,11 +1,21 @@
+#include <Arduino.h>
+
 
 uint32_t value = 10000;
-
+uint32_t bellow = 9000;
 
 
 void setup()
 {
+    Serial.begin(9600);
+    while (!Serial);
     
+    uint32_t delta = value - bellow;
+    Serial.println(delta);  // 1000
+
+    // Overflow (negative value)
+    delta = bellow - value;
+    Serial.println(delta);  // 4294966296
 }
 
 void loop()
