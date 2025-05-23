8a0b8b0c9bcfb52d64e2769d0af219699a2a9599
Rui Monteiro <ruiseixas@yahoo.com>
2025-05-23 08:00:25 +0100

    Updated pull git scripts

--- Git/pull_scripts/git_pull_all.bat
+++ Git/pull_scripts/git_pull_all.bat
@@ -2,4 +2,4 @@ git -C %USERPROFILE%\Documents\GitHub\JsonMidiCreator pull
 git -C %USERPROFILE%\Documents\GitHub\JsonMidiPlayer pull
 git -C %USERPROFILE%\Documents\GitHub\LearningConcepts pull
 git -C %USERPROFILE%\Documents\GitHub\JsonTalkie pull
-
+git -C %USERPROFILE%\Documents\GitHub\JsonTalkiePy pull
--- Git/pull_scripts/git_pull_all.sh
+++ Git/pull_scripts/git_pull_all.sh
@@ -2,4 +2,4 @@ git -C ~/GitHub/JsonMidiPlayer pull
 git -C ~/GitHub/JsonMidiCreator pull
 git -C ~/GitHub/LearningConcepts pull
 git -C ~/GitHub/JsonTalkie pull
-
+git -C ~/GitHub/JsonTalkiePy pull
